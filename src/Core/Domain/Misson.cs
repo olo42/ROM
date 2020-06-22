6833fa5def37e7d442eeb94888b11da6d66f9792
Olo42 <olo@apck.de>
2020-06-22 23:30:54 +0200

    Start design Misson entity

--- src/Core/Domain/Misson.cs
+++ src/Core/Domain/Misson.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
 
 namespace com.github.olo42.SAROnion.Core.Domain
 {
-    public class Misson
-    {
-    }
+  public class Misson : BaseEntity
+  {
+    public IEnumerable<Action> Actions { get; set; }
+    public Person Leader { get; set; }   
+    public string Objective { get; set; }
+    IEnumerable<Unit> Units { get; set; }
+    public IEnumerable<Contact> Contacts { get; set; }
+    public EMissionState State { get; set; }
+  }
 }
