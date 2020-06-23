05e554d80ffe9533115a79b51855f41104334d53
Olo42 <olo@apck.de>
2020-06-23 17:09:12 +0200

    Use generic interfaces and rename Run method to Execute

--- src/Core/Application/LogType/CreateLogType.cs
+++ src/Core/Application/LogType/CreateLogType.cs
@@ -7,22 +7,22 @@ using com.github.olo42.SAROnion.Core.Domain;
 
 namespace com.github.olo42.SAROnion.Core.Application
 {
-  public class CreateLogType
+  public class CreateLogType : ICreate<CreateLogTypeInput, LogType>
   {
-    private readonly ILogTypeRepository repository;
+    private readonly IRepository<LogType> repository;
 
-    public CreateLogType(ILogTypeRepository repository)
+    public CreateLogType(IRepository<LogType> repository)
     {
       this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
     }
 
-    public Task<LogType> Run(string name)
+    public Task<LogType> Execute(CreateLogTypeInput input)
     {
-      if (name is null) throw new ArgumentNullException(nameof(name));
+      var name = input?.Name ?? throw new ArgumentNullException(nameof(input.Name));
 
       var logType = new LogType();
       logType.Id = Guid.NewGuid().ToString();
-      logType.Name = name;
+      logType.Name = input.Name;
 
       repository.Write(logType);
 
--- src/Tests/Unit/CoreTests/CreateLogTypeTests.cs
+++ src/Tests/Unit/CoreTests/CreateLogTypeTests.cs
@@ -1,30 +1,31 @@
 // Copyright (c) Oliver Appel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using com.github.olo42.SAROnion.Core.Application;
-using com.github.olo42.SAROnion.Core.Domain;
 using Moq;
 using NUnit.Framework;
+using com.github.olo42.SAROnion.Core.Application;
+using com.github.olo42.SAROnion.Core.Domain;
 
 namespace com.github.olo42.SAROnion.Test.Unit.CoreTest
 {
   [TestFixture]
   public class CreateLogTypeTests
   {
-    private CreateLogType createAction;
-    private Mock<ILogTypeRepository> logTypeRepository;
+    private ICreate<CreateLogTypeInput, LogType> create;
+    private Mock<IRepository<LogType>> logTypeRepository;
 
     [SetUp]
     public void Setup()
     {
-      logTypeRepository = new Mock<ILogTypeRepository>();
-      createAction = new CreateLogType(logTypeRepository.Object);
+      logTypeRepository = new Mock<IRepository<LogType>>();
+      create = new CreateLogType(logTypeRepository.Object);
     }
 
     [Test]
     public void CreatesLogTypeObject()
     {
-      var result = createAction.Run("Type 1").Result;
+      var input = new CreateLogTypeInput { Name = "Action 1" };
+      var result = create.Execute(input).Result;
 
       Assert.That(result, Is.Not.Null);
     }
@@ -32,7 +33,8 @@ namespace com.github.olo42.SAROnion.Test.Unit.CoreTest
     [Test]
     public void AssignesNewId()
     {
-      var result = createAction.Run("Type 1").Result;
+      var input = new CreateLogTypeInput { Name = "Action 1" };
+      var result = create.Execute(input).Result;
 
       Assert.That(result.Id, Is.Not.Null);
     }
@@ -40,16 +42,16 @@ namespace com.github.olo42.SAROnion.Test.Unit.CoreTest
     [Test]
     public void RequiresNameParameter()
     {
-      string name = null;
+      var input = new CreateLogTypeInput { Name = null };
 
-      Assert.That(() => createAction.Run(name).Result, Throws.ArgumentNullException);
+      Assert.That(() => create.Execute(input).Result, Throws.ArgumentNullException);
     }
 
     [Test]
     public void AssignesName()
     {
-      var name = "Alert";
-      var result = createAction.Run(name).Result;
+      var input = new CreateLogTypeInput { Name = "Alert" };
+      var result = create.Execute(input).Result;
 
       Assert.That(result.Name, Is.EqualTo("Alert"));
     }
@@ -57,7 +59,8 @@ namespace com.github.olo42.SAROnion.Test.Unit.CoreTest
     [Test]
     public void StoresInRepository()
     {
-      var result = createAction.Run("Type 1").Result;
+      var input = new CreateLogTypeInput { Name = "Action 1" };
+      var result = create.Execute(input).Result;
 
       logTypeRepository.Verify(x => x.Write(result), Times.Once);
 
@@ -66,7 +69,7 @@ namespace com.github.olo42.SAROnion.Test.Unit.CoreTest
     [Test]
     public void RequiresRepositoryParameter()
     {
-      ILogTypeRepository repository = null;
+      IRepository<LogType> repository = null;
 
       Assert.That(() => new CreateLogType(repository), Throws.ArgumentNullException);
     }
