1d507a57d4c5ab33275df3f20d59b390bf47bc86
Olo42 <olo@apck.de>
2020-06-23 23:14:56 +0200

    Add generic IRead interface

---
+++ src/Core/Application/IRead.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+
+namespace com.github.olo42.SAROnion.Core.Application
+{
+  public interface IRead<TInput, TOutput>
+  {
+    TOutput Execute(TInput input);
+  }
+}
