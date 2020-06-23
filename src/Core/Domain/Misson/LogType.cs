3e644c26f82aa43a7662b06a64a7aabe7463561c
Olo42 <olo@apck.de>
2020-06-23 11:55:28 +0200

    Add LogType entity

--- src/Core/Domain/Misson/LogType.cs
+++ src/Core/Domain/Misson/LogType.cs
@@ -1,14 +1,10 @@
 // Copyright (c) Oliver Appel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-
 namespace com.github.olo42.SAROnion.Core.Domain
 {
-  public class LogEntry : BaseEntity
+  public class LogType : BaseEntity
   {
-    public LogType Type { get; set; }
-    public DateTime CreationDateTime { get; set; }
-    public string Message { get; set; }
+    public string Name { get; set; }
   }
 }
