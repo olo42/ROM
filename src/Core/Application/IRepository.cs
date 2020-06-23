54b5d4bcf56e4a72e7e54f764dc18b07924e06cd
Olo42 <olo@apck.de>
2020-06-23 23:15:36 +0200

    Add Read method to IRepository

--- src/Core/Application/IRepository.cs
+++ src/Core/Application/IRepository.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Oliver Appel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Threading.Tasks;
 
 namespace com.github.olo42.SAROnion.Core.Application
@@ -8,5 +9,6 @@ namespace com.github.olo42.SAROnion.Core.Application
   public interface IRepository<T>
   {
     Task Write(T input);
+    Task<T> Read(string id);
   }
 }
