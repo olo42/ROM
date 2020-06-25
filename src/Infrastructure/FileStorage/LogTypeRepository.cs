f32857e56049ea5c518ea8439be9430c2ba889d5
Olo42 <olo@apck.de>
2020-06-25 21:49:08 +0200

    Implement LogTypeRepository

---
+++ src/Infrastructure/FileStorage/LogTypeRepository.cs
@@ -0,0 +1,50 @@
+
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using com.github.olo42.SAROnion.Core.Domain;
+
+namespace com.github.olo42.SAROnion.Infrastructure.FileStorage
+{
+  public class LogTypeRepository : BaseRepository<LogType>
+  {
+    public LogTypeRepository(string directoryPath) : base(directoryPath) { }
+
+    public override async Task<LogType> Read(string id)
+    {
+      var logTypes = await this.Read();
+
+      return logTypes.ToList().Find(x => x.Id == id);
+    }
+
+    public override async Task Write(LogType input)
+    {
+      var logTypes = await AddOrUpdateAsync(input);
+      var json = JsonSerializer.Serialize(logTypes);
+      await File.WriteAllTextAsync(base.GetFilePath(), json);
+    }
+
+    private async Task<List<LogType>> AddOrUpdateAsync(LogType input)
+    {
+      var logTypes = (await this.Read()).ToList();
+      var origin = logTypes.Find(x => x.Id == input.Id);
+
+      if (origin == null)
+        logTypes.Add(input);
+      else
+        this.Update(origin, input);
+
+      return logTypes;
+    }
+
+    private void Update(LogType origin, LogType input)
+    {
+      origin.Name = input.Name;
+    }
+  }
+}
---
+++ src/Tests/Unit/Infrastructure/FileStorage/LogTypeRepositoryTests.cs
@@ -0,0 +1,78 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using com.github.olo42.SAROnion.Core.Domain;
+using com.github.olo42.SAROnion.Infrastructure.FileStorage;
+using NUnit.Framework;
+
+namespace com.github.olo42.SAROnion.Test.Unit.Infrastructure.FileStorage
+{
+  [TestFixture]
+  public class LogTypeRepositoryTests
+  {
+    private string filePath;
+    private LogTypeRepository repository;
+
+    [SetUp]
+    public void Setup()
+    {
+      var path = Path.Combine(Path.GetTempPath(), "FileStorage");
+      if(!Directory.Exists(path)) 
+        Directory.CreateDirectory(path);
+
+      repository = new LogTypeRepository(path);
+
+      this.filePath = filePath = Path.Combine(path, this.repository.GetType().Name + ".dat");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      var path = Path.Combine(Path.GetTempPath(), "FileStorage");
+      if(Directory.Exists(path)) {}
+        Directory.Delete(path, true);
+    }
+
+    [Test]
+    public async Task Write_SerializesAsync()
+    {
+      var logType = new LogType { Id = "abcdefg", Name = "My First LogType" };
+
+      await this.repository.Write(logType);
+
+      var content = GetFileContent(this.filePath);
+      var expectedJsonString = "[{\"Name\":\"My First LogType\",\"Id\":\"abcdefg\"}]";  
+
+      Assert.That(content, Is.EqualTo(expectedJsonString));
+    }
+
+    [Test]
+    public async Task Write_UpdatesAsync()
+    {
+      var origin = new List<LogType> { new LogType { Id = "abcdefg", Name = "My First LogType" } };
+      SerializeToFile(origin);
+
+      var update = new LogType { Id = "abcdefg", Name = "My Updated LogType" };
+      await this.repository.Write(update);
+      string content = GetFileContent(this.filePath);
+
+      var expectedJsonString = "[{\"Name\":\"My Updated LogType\",\"Id\":\"abcdefg\"}]";
+      Assert.That(content, Is.EqualTo(expectedJsonString));
+    }
+
+    private string GetFileContent(string path)
+    {
+      return File.ReadAllText(path);
+    }
+
+    private void SerializeToFile(List<LogType> origin)
+    {
+      var json = JsonSerializer.Serialize(origin);
+      File.WriteAllText(this.filePath, json);
+    }
+  }
+}
