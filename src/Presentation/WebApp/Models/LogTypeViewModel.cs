e2b81af9cc92555b6288cf55b31e436ed826ea14
Olo42 <olo@apck.de>
2020-06-26 15:07:42 +0200

    Add Create LogType to WebApp

---
+++ src/Presentation/WebApp/Models/LogTypeViewModel.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace com.github.olo42.SAROnion.Presentation.WebApp.Pages
+{
+  public class LogTypeViewModel
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+  }
+}
---
+++ src/Presentation/WebApp/Pages/LogTypes/Create.cshtml.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using com.github.olo42.SAROnion.Core.Domain;
+using com.github.olo42.SAROnion.Core.Application;
+
+namespace com.github.olo42.SAROnion.Presentation.WebApp.Pages.LogTypes
+{
+  public class CreateModel : PageModel
+    {
+        private readonly ICreate<string, LogType> _action;
+
+        public CreateModel(ICreate<string, LogType> action)
+        {
+            _action = action;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public LogTypeViewModel LogTypeViewModel { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _action.Execute(LogTypeViewModel.Name);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
